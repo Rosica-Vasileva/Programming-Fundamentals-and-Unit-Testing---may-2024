b24a92b7243b7c95e73a2b3243ba07e7d6b588d4
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-20 02:07:35 +0300

    Add files via upload

---
+++ 02.Visual Studio/05.Town Info/Program.cs
@@ -0,0 +1,10 @@
+﻿//INPUT Chiang Mai Thailand 
+//OUTPUT Chiang Mai is in Thailand
+
+//INPUT Sofia Bulgaria 
+//OUTPUT Sofia is in Bulgaria
+
+string townName = Console.ReadLine();
+string country = Console.ReadLine();
+
+Console.WriteLine($"{townName} is in {country}");
