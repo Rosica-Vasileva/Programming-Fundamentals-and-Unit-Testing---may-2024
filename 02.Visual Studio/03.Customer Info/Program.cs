688bd14e194cbf61694ac06058322261af266fae
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-20 01:46:52 +0300

    Add files via upload

---
+++ 02.Visual Studio/03.Customer Info/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//INPUT Desislava Topuzakova d.topuzakova@softuni.bg 
+//OUTPUT Customer: Desislava Topuzakova(dtopuzakova @softuni.bg)
+//INPUT Ivan Ivanov i.ivanov@abv.bg
+//OUTPUT Customer: Ivan Ivanov(i.ivanov @abv.bg)
+
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string email = Console.ReadLine();
+
+Console.WriteLine($"Customer:{firstName} {lastName} ({email})");
