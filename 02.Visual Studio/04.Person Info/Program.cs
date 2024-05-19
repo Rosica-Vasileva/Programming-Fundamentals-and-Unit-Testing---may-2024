5e56b4eda3ee7a679884229cd86782bb75a06f44
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-20 02:01:29 +0300

    Add files via upload

---
+++ 02.Visual Studio/04.Person Info/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+//INPUT Desislava Topuzakova Bulgaria Stara Zagora 
+//OUTPUT Desislava Topuzakova from Bulgaria – Stara Zagora!
+
+//INPUT Ivan Ivanov England London 
+//OUTPUT Ivan Ivanov from England – London!
+
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string country = Console.ReadLine();
+string town = Console.ReadLine();
+
+Console.WriteLine($"{firstName} {lastName} from {country} - {town}");
