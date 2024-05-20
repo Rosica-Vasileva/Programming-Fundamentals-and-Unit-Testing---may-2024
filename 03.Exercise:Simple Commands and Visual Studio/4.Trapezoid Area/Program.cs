cee79daf2b83842832dd7eb95561913c798e89c5
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-20 14:03:23 +0300

    Add files via upload

---
+++ 03.Exercise:Simple Commands and Visual Studio/4.Trapezoid Area/Program.cs
@@ -0,0 +1,11 @@
+﻿//Input  Output
+//6
+//2      12
+//3      
+
+int firstBase = int.Parse(Console.ReadLine());
+int secondBase = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+int area = (firstBase + secondBase) / 2 * height;
+Console.WriteLine(area);
