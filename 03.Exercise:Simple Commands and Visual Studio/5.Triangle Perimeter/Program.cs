6e6f6dc58f199a0437febf2d3053f84690fe2f49
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-20 14:04:21 +0300

    Add files via upload

---
+++ 03.Exercise:Simple Commands and Visual Studio/5.Triangle Perimeter/Program.cs
@@ -0,0 +1,11 @@
+﻿//Input Output
+//6
+//2     11
+//3 
+
+int sideA = int.Parse(Console.ReadLine());
+int sideB = int.Parse(Console.ReadLine());
+int sideC = int.Parse(Console.ReadLine());
+
+int perimeters = sideA + sideB + sideC;
+Console.WriteLine(perimeters);
