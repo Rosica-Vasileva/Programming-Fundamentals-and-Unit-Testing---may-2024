2e85ce03402abcfa8811bf083e070f3e0d2d6eba
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-21 23:29:07 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/01.Days to minutes/Program.cs
@@ -0,0 +1,13 @@
+﻿//Input    Output
+//2 Minutes = 2880
+//5 Minutes = 7200
+//7 Minutes = 10080
+
+//INPUT
+int days = int.Parse(Console.ReadLine());
+
+//ACT 
+int minutes = days * 24 * 60;
+
+//OUTPUT
+Console.WriteLine($"Minutes={minutes}");
