dcb67fcef20755cb5fc85a671b95264cf6662411
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-21 23:53:22 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/03.Circle area and perimeter/Program.cs
@@ -0,0 +1,14 @@
+﻿//Input   Output
+//7        Area = 153.94 
+//         Perimeter = 43.98
+
+//INPUT
+double radiusOfaCircle = double.Parse(Console.ReadLine());
+
+//ACT
+double area = radiusOfaCircle * radiusOfaCircle * Math.PI;
+double perimeter = 2 * Math.PI * radiusOfaCircle;
+
+//OUTPUT
+Console.WriteLine($"{area:F2}");
+Console.WriteLine($"{perimeter:F2}");
