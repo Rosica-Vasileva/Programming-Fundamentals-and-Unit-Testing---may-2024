dcc69e90816140edb64b45117ad3883fb69dd059
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-21 23:42:15 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/02.Calculate Speed/Program.cs
@@ -0,0 +1,15 @@
+﻿//Input Output
+//15
+//2     7.50
+//15
+//2.2   6.82
+
+//INPUT
+double distance = double.Parse(Console.ReadLine());
+double time = double.Parse(Console.ReadLine());
+
+//ACT
+double speed = distance / time;
+
+//OUTPUT
+Console.WriteLine($"{speed:F2}");
