45fbc731f5ae1139435c876ca227e227ddb09798
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-22 23:02:24 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/07.Projects Creation/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//George
+//4 
+//OUTPUT The architect George will need 12 hours to complete 4 project/s.
+//Sanya
+//9 
+//OUTPUT The architect Sanya will need 27 hours to complete 9 project/s.
+
+//INPUT
+using System.Runtime.InteropServices;
+
+string name = Console.ReadLine();
+int countOfprojects = int.Parse(Console.ReadLine());
+//ACT
+int neededHours = countOfprojects * 3;
+//OUTPUT
+Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {countOfprojects} project/s.");
