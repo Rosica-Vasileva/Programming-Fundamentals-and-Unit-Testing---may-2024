25fc4c2c4a4f77f9f6c6c7119f53830171bc57b1
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-22 22:08:20 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/04.Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿//Input   Output
+//1852.4  1.85
+//798.3   0.80
+
+//INPUT
+using System.Diagnostics.Metrics;
+
+double meters = double.Parse(Console.ReadLine());
+
+//ACT
+double kilometers = meters/ 1000;
+
+//OUTPUT
+Console.WriteLine($"{kilometers:F2}");
