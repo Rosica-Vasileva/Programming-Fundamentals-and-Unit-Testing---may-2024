d7d30b59833dee670f211f3f7047c8213f3b4999
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-22 22:41:19 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/05.Convert Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,12 @@
+﻿//Input Output
+//37    98.60
+//100   212.00
+
+//INPUT
+double degreesCelsius = double.Parse(Console.ReadLine());
+
+//ACT
+double degreesFahrenheit = degreesCelsius * 1.8 + 32;
+
+//OUTPUT
+Console.WriteLine($"{degreesFahrenheit:F2}");
