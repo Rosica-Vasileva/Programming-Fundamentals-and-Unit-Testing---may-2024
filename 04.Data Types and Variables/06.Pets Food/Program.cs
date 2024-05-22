1d4d17bd18ab3de771959d94df0accad00d0d2c0
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-22 23:01:59 +0300

    Add files via upload

---
+++ 04.Data Types and Variables/06.Pets Food/Program.cs
@@ -0,0 +1,13 @@
+﻿//Input  Output
+//5
+//4      28.50 lv.
+//13
+//9      68.50 lv.
+
+//INPUT
+int dogFoodcount = int.Parse(Console.ReadLine());
+int catFoodcount = int.Parse(Console.ReadLine());
+//ACT
+double expenses = dogFoodcount * 2.5 + catFoodcount * 4.00;
+//OUTPUT
+Console.WriteLine($"{expenses:F2} lv.");
