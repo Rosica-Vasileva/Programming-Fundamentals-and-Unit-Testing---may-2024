0c1a93f8504d345fe9b078da96491d6148741daf
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-24 23:51:12 +0300

    Add files via upload

---
+++ 05.Simple Commands/03.Market/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+//Input   Output 
+//42.50   249.69 
+//3.30
+//60.80
+//1.80 
+
+//INPUT
+decimal tomatoPrice = decimal.Parse(Console.ReadLine());
+decimal tomato = decimal.Parse(Console.ReadLine());
+decimal cucumberPrice = decimal.Parse(Console.ReadLine());
+decimal cucumber = decimal.Parse(Console.ReadLine());
+
+//ACT
+decimal tomatoCost = tomatoPrice * tomato;
+decimal cucumberCost = cucumberPrice * cucumber;
+
+decimal totalCost = tomatoCost + cucumberCost;
+Console.WriteLine($"{totalCost:F2}");
