65d48d03a439b9740b8d05c03df6fe7ce40979da
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-24 13:40:47 +0300

    Add files via upload

---
+++ 05.Simple Commands/01.Convertor:  USD to EURO/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Input Output
+//17    14.96
+//87.2   76.74
+
+//INPUT 
+decimal usd = decimal.Parse(Console.ReadLine());
+//ACT
+decimal usdEuroRate = 0.88m;
+decimal euro = usd* usdEuroRate;
+//OUTPUT
+Console.WriteLine($"{euro:F2}");
