9a3738cf7d56dc5d83a0c73bceee34fa08f45d03
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-25 00:59:49 +0300

    Add files via upload

---
+++ 05.Simple Commands/04.Tiles/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Input  Output 
+//3.3
+//2.2
+//0.25
+//0.75    43
+
+//INPUT 
+
+double bathH=double.Parse(Console.ReadLine());
+double bathW = double.Parse(Console.ReadLine());
+
+double tileH=double.Parse(Console.ReadLine());
+double tileW = double.Parse(Console.ReadLine());
+
+//the area of the bathroom and the tiles
+
+double bathArea = bathW * bathH * 1.10; // add 10% surplus
+double tileArea = tileH * tileW;
+
+//we find how many tiles
+double tilesNeeded = bathArea / tileArea;
+
+Console.WriteLine($"{tilesNeeded:F0}");
