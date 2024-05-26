c4724f68560f89304466f51d15c70cd78b2f975e
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-26 14:07:03 +0300

    Add files via upload

---
+++ 05.Simple Commands/05.Deposit Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿//amount = deposited amount + term of deposit * (deposited amount * annual interest rate) / 12
+//1. Deposited amount – real number in the range [100.00 ... 10000.00]
+//2.Term of the deposit (in months) – an integer in the range [1... 12]
+//3.Annual interest rate – real number in the range [0.00 ... 100.00]
+
+//INPUT
+
+decimal depositedAmount = decimal.Parse(Console.ReadLine());
+int depositTerm = int.Parse(Console.ReadLine());
+decimal interestRate = decimal.Parse(Console.ReadLine());
+
+//ACT
+
+decimal annualInterest = depositedAmount * interestRate / 100;
+decimal montlyInterest = annualInterest / 12;
+decimal amount = depositedAmount + (depositTerm * montlyInterest);
+
+//OUTPUT
+
+Console.WriteLine(amount);
