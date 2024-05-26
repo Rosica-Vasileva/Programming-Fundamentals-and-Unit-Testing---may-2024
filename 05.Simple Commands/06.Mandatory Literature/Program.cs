232a7f89e1d89f3a73e3544fd2e682adf86528ba
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-26 14:18:32 +0300

    Add files via upload

---
+++ 05.Simple Commands/06.Mandatory Literature/Program.cs
@@ -0,0 +1,18 @@
+﻿//1. Number of pages in the current book – an integer in the range [1... 1000].
+//2. Pages that he reads in 1 hour – an integer in the range [1... 1000].
+//3. The number of days he needs to finish the book – an integer in the range [1... 1000].1. Number of pages in the current book – an integer in the range [1... 1000].
+
+//INPUT 
+
+int pages = int.Parse(Console.ReadLine());
+int pagesReadOneHour = int.Parse(Console.ReadLine());
+int numberOfDay = int.Parse(Console.ReadLine());
+
+//ACT
+
+int totalReadingtime = pages / pagesReadOneHour;
+int requiredPerDay = totalReadingtime / numberOfDay;
+
+//OUTPUT 
+
+Console.WriteLine(requiredPerDay);
