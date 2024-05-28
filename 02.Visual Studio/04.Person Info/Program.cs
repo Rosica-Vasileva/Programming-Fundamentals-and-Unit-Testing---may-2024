a7f08e2b823f0f414f0beb2e9ad8242f67535d7c
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-28 22:14:28 +0300

    Update Program.cs

--- 02.Visual Studio/04.Person Info/Program.cs
+++ 02.Visual Studio/04.Person Info/Program.cs
@@ -11,4 +11,4 @@ string lastName = Console.ReadLine();
 string country = Console.ReadLine();
 string town = Console.ReadLine();
 
-Console.WriteLine($"{firstName} {lastName} from {country} - {town}");
+Console.WriteLine($"{firstName} {lastName} from {country} - {town}!");
