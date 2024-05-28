02a223f046a28a6041e7a06649872acc971e2d1d
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-28 14:52:30 +0300

    Add files via upload

---
+++ 06.Exercise:Data Type, Variables and Simple Calculations/04.Food Delivery/Program.cs
@@ -0,0 +1,32 @@
+﻿//• Chicken menu – 10.35 lv.
+//• Menu with fish – 12.40 lv.
+//• Vegetarian menu – 8.15 lv.
+//Number of chicken menus – integer in the range [0 ... 99]
+//Number of menus with fish – integer in the range [0 ... 99]
+//Number of vegetarian menus – an integer in the range [0 ... 99]
+
+//INPUT
+
+double chickenPriceMenu = 10.35;
+double fishPriceMenu = 12.40;
+double vegetarianPriceMenu = 8.15;
+double deliveryPrice = 2.5;
+double dessert = 0;
+
+int chickenMenuCosts = int.Parse(Console.ReadLine());
+int fishMenuCosts = int.Parse(Console.ReadLine());
+int vegetarianMenuCosts = int.Parse(Console.ReadLine());
+
+//ACT
+
+double allChickenPrice = chickenMenuCosts * chickenPriceMenu;
+double allFishPrice = fishMenuCosts * fishPriceMenu;
+double allVegetarianPrice = vegetarianMenuCosts * vegetarianPriceMenu;
+
+double totalCostsOfMenu = allChickenPrice + allFishPrice + allVegetarianPrice;
+double dessertPrice = totalCostsOfMenu * 0.2;
+double totalOrderPrice = totalCostsOfMenu + dessertPrice + deliveryPrice;
+
+Console.WriteLine(totalOrderPrice);
+
+
