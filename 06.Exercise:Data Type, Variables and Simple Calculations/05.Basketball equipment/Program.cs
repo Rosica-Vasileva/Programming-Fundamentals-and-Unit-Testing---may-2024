3e030c51a7c9845adcc31f2b3d2755e0d61913e2
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-28 15:02:14 +0300

    Add files via upload

---
+++ 06.Exercise:Data Type, Variables and Simple Calculations/05.Basketball equipment/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+//· Basketball sneakers – their price is 40% less than the fee for one year
+//· Basketball uniform – its price is 20% cheaper than that of sneakers
+//· Basketball ball – its price is 1 / 4 of the price of the basketball uniform
+//· Basketball accessories – their price is 1 / 5 of the price of the basketball ball
+
+//INPUT  OUTPUT
+//365    811.76
+//550    1223.2
+
+//INPUT 
+
+int feeForOneYear = int.Parse(Console.ReadLine());
+
+//ACT
+
+double sneakersPrice = feeForOneYear *0.6; //60%
+double uniformPrice = sneakersPrice * 0.8; //80%
+double ballPrice = uniformPrice / 4;
+double accessoriesPrice = ballPrice / 5;
+double totalPrice = feeForOneYear + sneakersPrice + uniformPrice + ballPrice + accessoriesPrice;
+
+//OUTPUT
+
+Console.WriteLine(totalPrice);
