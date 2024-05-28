1f7cb79419a1807df3f0170e7a22452b027f965c
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-28 15:39:28 +0300

    Add files via upload

---
+++ 06.Exercise:Data Type, Variables and Simple Calculations/06.Aquarium/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Input Output 
+//85
+//75
+//47
+//17    248.69 
+
+//105
+//77
+//89
+//18.5  586.45
+
+//INPUT
+
+int lengthInCm = int.Parse(Console.ReadLine());
+int widthInCm = int.Parse(Console.ReadLine());
+int heighInCm = int.Parse(Console.ReadLine());
+double  percentage = double.Parse(Console.ReadLine());
+
+//ACT
+
+int volume = lengthInCm * widthInCm * heighInCm;
+double volumeInLiters = volume /1000;
+double requiredLiters = volumeInLiters * (1 - percentage/100);
+
+//OUTPUT
+
+Console.WriteLine($"{requiredLiters:F2}");
