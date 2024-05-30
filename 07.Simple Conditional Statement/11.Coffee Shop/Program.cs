f0cdbe10a9bcb611bba5dc77364a6580ba6b2325
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-30 23:29:56 +0300

    Add files via upload

---
+++ 07.Simple Conditional Statement/11.Coffee Shop/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string drinkName = Console.ReadLine();
+string extra = Console.ReadLine();
+
+
+decimal price = 0;
+if (drinkName == "coffee")
+{
+    price = 1.00m;
+}
+else
+{
+    price = 0.60m;
+}
+
+if (extra == "sugar")
+{
+    price += 0.40m;
+}
+
+Console.WriteLine($"Final price:${price:F2}");
