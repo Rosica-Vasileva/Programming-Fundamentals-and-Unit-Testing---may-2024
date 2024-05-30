665c240b9022cc0e18effc08a7413944e04c23a3
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-30 23:10:17 +0300

    Add files via upload

---
+++ 07.Simple Conditional Statement/10.Valid Triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a >= b + c)
+{
+    Console.WriteLine("Invalid Traingle");
+}
+
+else if (b>=a+c)
+{
+    Console.WriteLine("Invalid Tringle");
+}       
+
+else if (c>=a+b)
+{
+    Console.WriteLine("Invalid Tringle");
+}
+
+else
+{
+    Console.WriteLine("Valid Tringle");
+}
