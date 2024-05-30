25ee7b7a623c1c2f8bac97b0fe003a0e936d9824
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-30 13:51:17 +0300

    Add files via upload

---
+++ 07.Simple Conditional Statement/09. Area of Figures/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+string figureArea = Console.ReadLine();
+
+if (figureArea == "square")
+{
+    double side = double.Parse(Console.ReadLine());
+    double area = side * side;
+    Console.WriteLine($"{area:F2}");
+}
+
+else if (figureArea == "rectangle")
+{
+    double length = double.Parse(Console.ReadLine());
+    double width = double.Parse(Console.ReadLine());
+    double area = length * width;
+    Console.WriteLine($"{area:F2}");
+}
+
+else if (figureArea == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    double area = Math.PI * radius * radius; 
+    Console.WriteLine($"{area:F2}"); 
+}
