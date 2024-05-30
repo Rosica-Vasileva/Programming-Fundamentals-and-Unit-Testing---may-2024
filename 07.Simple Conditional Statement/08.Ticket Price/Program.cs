ae199546d5d05267783ececbc4e4d9dd27978077
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-30 13:25:10 +0300

    Add files via upload

---
+++ 07.Simple Conditional Statement/08.Ticket Price/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string ticketType = Console.ReadLine();
+
+if (ticketType=="regular")
+{
+    decimal price = 1.00m;
+    Console.WriteLine($"{price:F2}");
+}
+
+else if (ticketType=="student")
+{
+    decimal price = 1.60m;
+    Console.WriteLine($"{price:F2}");
+}
+
+else
+{
+    Console.WriteLine("Invalid ticket type");
+}
