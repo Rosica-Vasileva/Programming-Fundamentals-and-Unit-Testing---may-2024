f3f04e2946403a09d978bbd71d4c9fb25e8843b9
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-05-31 23:26:27 +0300

    Add files via upload

---
+++ 08.Complex Conditional Statement/01.Marketplace/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+//INPUT 
+//product and day , print the price 
+
+string product = Console.ReadLine();
+string day = Console.ReadLine();
+decimal price = 0;
+
+if (product == "Banana")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine($"{2.50:F2}");
+    }
+    else if (day == "Weekend")
+    {
+        Console.WriteLine($"{2.70:F2}");
+    }
+}
+
+else if (product == "Apple")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine($"{1.30:F2}");
+    }
+    else if (day == "Weekend")
+    {
+        Console.WriteLine($"{1.60:F2}");
+    }
+
+}
+else if (product == "Kiwi")
+{
+    if (day == "Weekday")
+    {
+        Console.WriteLine($"{2.20:F2}");
+    }
+    else if (day == "Weekend")
+    {
+        Console.WriteLine($"{3.30:F2}");
+    }
+}
