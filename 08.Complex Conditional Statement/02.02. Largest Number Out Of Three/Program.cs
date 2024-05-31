b62aed053573517e0214e3615e145292cce7dab7
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-01 00:00:45 +0300

    Add files via upload

---
+++ 08.Complex Conditional Statement/02.02. Largest Number Out Of Three/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine(a);
+    }
+}
+
+else if (b > c)
+{
+    Console.WriteLine(b);
+
+}
+else
+{
+    Console.WriteLine(c
+        );
+}
