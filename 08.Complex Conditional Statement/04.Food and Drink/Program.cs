051814e930e3b52c287e58a8427e871e86b9dbdb
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-03 23:02:31 +0300

    Add files via upload

---
+++ 08.Complex Conditional Statement/04.Food and Drink/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string product = Console.ReadLine();
+
+if(product=="curry" || product=="noodles" || product=="bread" || product=="sushi" || product == "spaghetti")
+{
+    Console.WriteLine("Food");
+}
+else if (product=="tea" || product=="coffee" || product=="water" || product == "juice")
+{
+    Console.WriteLine("Drink");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
