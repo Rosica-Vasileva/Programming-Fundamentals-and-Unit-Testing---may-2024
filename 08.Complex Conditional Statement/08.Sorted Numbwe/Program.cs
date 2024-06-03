dcb13e7082f86f21fc288d28828a3e6172c504e0
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-03 23:55:04 +0300

    Add files via upload

---
+++ 08.Complex Conditional Statement/08.Sorted Numbwe/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+//Write a program, which checks for sorted 3 numbers:
+//Reads 3 integer numbers from the console
+//Based on the order of the numbers, print:
+//"Ascending" – if the numbers are in ascending order (from the smallest to the largest number)
+//"Descending" – if the numbers are in descending order (from the largest to the smallest number)
+//"Not sorted" – in any other case
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+bool isAsending = a < b && b < c;
+bool isDesending = a > b && b > c;
+
+if (isAsending)
+{
+    Console.WriteLine("Ascending");
+}
+else if (isDesending)
+{
+    Console.WriteLine("Descending");
+}
+else
+{
+    Console.WriteLine("Not sorted");
+}
