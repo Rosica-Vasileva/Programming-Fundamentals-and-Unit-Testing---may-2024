6a4852efe0f95590d2b12b276176a83ce06f70b2
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-04 00:28:34 +0300

    Add files via upload

---
+++ 08.Complex Conditional Statement/09.Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿//Write a program, that calculates the price for all the tickets for a cinema movie:
+//Reads the type of the movie (string), the count of the rows (an integer number) and count of the seats per row (an integer number) in the cinema
+//Type of the movie will be one of the following: "Premiere", "Normal" and "Discount"
+//Prints the total price for all seats formatted to the 2nd digit after the decimal point, based on the table below:
+
+//Normal
+//12
+//9
+
+
+string movieType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int seats = int.Parse(Console.ReadLine());
+int totalSeats = rows * seats;
+decimal price = 0.00m;
+
+if (movieType == "Premier")
+{
+    price = 12.00m;
+}
+else if (movieType == "Normal")
+{
+    price = 7.50m;
+}
+else
+{
+    price = 5.00m;
+}
+
+decimal totalPrice = totalSeats * price;
+Console.WriteLine($"{totalPrice:F2}");
