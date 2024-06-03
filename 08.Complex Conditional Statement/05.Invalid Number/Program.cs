4dc71656c915cec2b580d7d82948c7aaa50389c8
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-03 23:20:08 +0300

    Add files via upload

---
+++ 08.Complex Conditional Statement/05.Invalid Number/Program.cs
@@ -0,0 +1,13 @@
+﻿//75  invalid  150  (няма изход)  0 (няма изход)
+
+
+int number = int.Parse(Console.ReadLine());
+
+if (!((number >= 100 && number <= 200) || (number == 0)))
+{
+    Console.WriteLine("invalid");
+}
+else
+{
+    Console.WriteLine();
+}
