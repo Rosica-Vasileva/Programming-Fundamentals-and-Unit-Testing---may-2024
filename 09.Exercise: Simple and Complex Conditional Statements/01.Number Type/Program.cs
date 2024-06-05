9926e1e21ef8c18d859b9dbeab022a360ddf2cca
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-05 23:19:35 +0300

    Add files via upload

---
+++ 09.Exercise: Simple and Complex Conditional Statements/01.Number Type/Program.cs
@@ -0,0 +1,19 @@
+﻿//Input  Output     Input  Output     Input  Output
+//2      positive   -1     negative   0      zero
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("negative");
+}
+
+else if (number > 0)
+{
+    Console.WriteLine("positive");   
+}
+
+else //(number=0)
+{
+    Console.WriteLine("zero");
+}
