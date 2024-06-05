a6f7b5e407a6955f4eea2e17b8239a6f96294409
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-06 00:42:08 +0300

    Add files via upload

---
+++ 09.Exercise: Simple and Complex Conditional Statements/04.Number Operation/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//Write a program to evaluate operations, which:
+//Reads 2 real numbers and math operator (string) from the console
+//Possible given values for the math operator are: "+", "-", "*", "/"
+//Apply the operator with given numbers
+//Print output in the following format, where result is formatted to the second digit:
+//"{N1} {operator} {N2} = {result}"
+
+//INPUT 
+
+double numOne = double.Parse(Console.ReadLine());
+double numTwo = double.Parse(Console.ReadLine());
+char operation=char.Parse(Console.ReadLine());
+
+double result = 0;
+//ACT
+if (operation == '+')
+{
+    result = numOne + numTwo;
+}
+else if (operation == '-')
+{
+    result = numOne - numTwo;
+}
+else if (operation == '*')
+{
+    result = numOne * numTwo;
+}
+else if (operation == '/')
+{
+    result = numOne / numTwo;
+}
+//OUTPUT
+Console.WriteLine($"{numOne} {operation} {numTwo} = {result:F2}");
