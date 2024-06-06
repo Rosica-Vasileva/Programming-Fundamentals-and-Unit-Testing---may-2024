c287d6b3612932af66622e3a2af09bb6a51d9510
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-06 23:39:58 +0300

    Add files via upload

---
+++ 09.Exercise: Simple and Complex Conditional Statements/06.6. Product of Three Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Write a program that calculates the sign of the product of three numbers:
+//Reads 3 real numbers from the console
+//Print the sign of the product of the three given numbers: "positive", "negative" or "zero"
+//Note: Try to do this without multiplying the numbers.
+
+double num1 = double.Parse(Console.ReadLine());
+double num2 = double.Parse(Console.ReadLine());
+double num3 = double.Parse(Console.ReadLine());
+
+if (num1==0 || num2==0 || num3==0)
+{
+    Console.WriteLine("zero");
+}
+
+else if ((num1>0 && num2>0 && num3>0)||
+    (num1<0 && num2<0 && num3>0)||
+    (num1<0 && num2>0 && num3<0)||
+    (num1>0 && num2<0 && num3 < 0))
+{
+    Console.WriteLine("positive");
+}
+
+else
+{
+    Console.WriteLine("negative");
+}
