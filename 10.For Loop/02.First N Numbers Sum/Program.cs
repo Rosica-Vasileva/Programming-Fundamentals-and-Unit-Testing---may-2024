12aee78157710797d3201da3f8ef3a2bcb80d64b
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-10 01:34:55 +0300

    Add files via upload

---
+++ 10.For Loop/02.First N Numbers Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.Design;
+
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+string expression = "";
+
+for (int i = 1; i <= n; i++)
+{
+    sum += i;
+    if (i == n)
+    {
+        expression += $"{i}=";
+    }
+    else
+    {
+
+        expression += $"{i}+";
+
+    }
+}
+
+Console.WriteLine($"{expression}{sum}");
