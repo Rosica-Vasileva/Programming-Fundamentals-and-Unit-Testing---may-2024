be3579f7be6e14e078bcacba7006047403443c65
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-12 00:54:07 +0300

    Add files via upload

---
+++ 10.For Loop/04.Numbers Ending in 7/Program.cs
@@ -0,0 +1,11 @@
+﻿//Write a program that prints numbers ending in 7 in given range:
+//Reads an integer number n – end of the range
+//Prints all numbers from 7 to n, ending in 7
+
+//INPUT
+int n = int.Parse(Console.ReadLine());
+//ACT
+for (int i = 7; i <= n; i += 10)
+{   //OUTPUT
+    Console.WriteLine(i);
+}
