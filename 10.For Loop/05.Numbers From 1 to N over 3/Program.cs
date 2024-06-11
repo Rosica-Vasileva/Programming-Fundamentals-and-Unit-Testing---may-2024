1d1517433883b1ece09e2c1dd5a777b4397d563a
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-12 01:17:59 +0300

    Add files via upload

---
+++ 10.For Loop/05.Numbers From 1 to N over 3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Write a program that prints numbers in given range over 3:
+//· Reads an integer number n – end of the range
+//· Prints all numbers from 1 to n, over 3 (inclusively)
+
+//INPUT 
+
+int n = int.Parse(Console.ReadLine());
+//ACT
+for(int i = 1; i < n; i += 3)
+{    //OUTPUT
+    Console.WriteLine(i);
+}
