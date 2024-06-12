fb00a820c7554a9ecffb2229807eb549312a5295
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-12 12:08:28 +0300

    Add files via upload

---
+++ 10.For Loop/06.Exam Countdown/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Write a program to print a countdown to an exam:
+//· Read an integer number d: the count of days before an exam
+//· For each day d…1 print: "{currentDay} days before the exam"
+//· At the end print: "The exam has come
+
+//INPUT
+
+int days = int.Parse(Console.ReadLine());
+
+//ACT
+for (int i=days;i>=1;i--)
+{
+    Console.WriteLine($"{i} days before the exam");
+}
+
+Console.WriteLine("The exam has come.");
