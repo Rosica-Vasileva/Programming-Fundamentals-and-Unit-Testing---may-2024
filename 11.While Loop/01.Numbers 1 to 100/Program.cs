9c52c91d2b2d267f0133e8411a8adad4fa05f40b
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-16 00:28:05 +0300

    Add files via upload

---
+++ 11.While Loop/01.Numbers 1 to 100/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write a program that:
+//· Print the numbers from 1 to 100, each on separate line
+
+//for (int i=0;i<=100;i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+int i = int.Parse(Console.ReadLine());
+while (i<=100)
+{
+    Console.WriteLine(i);
+    i++;
+}
