ccd35c6dfdf636302781b9cd253ea3c2a8172931
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-16 00:29:12 +0300

    Add files via upload

---
+++ 11.While Loop/02.Decreasing Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿//Write a program that:
+//· Reads an integer number N
+//· Print the numbers from N down to 1 (inclusively), each on separate line
+//Note: N will always be bigger than 1.
+
+//int i =int.Parse(Console.ReadLine());
+//for (int i = n; 1 >= 1; i--;){
+//Console.WriteLine(i);}
+
+int n = int.Parse(Console.ReadLine());
+while (n >= 1)
+{
+    Console.WriteLine(n);
+    n--;
+}
