f1bade6b50a25f6b26f1d7ab7114d31b0526092d
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-16 00:31:05 +0300

    Add files via upload

---
+++ 11.While Loop/04.Sequence 2K+1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+//Write a program to print a sequence of numbers:
+
+//· The first number is 1
+
+//· Each next number is 2 times the previous number + 1
+
+//· Read an integer number n from the console – the max number
+
+//· Print the elements of the sequence (starting with 1), which are ≤ n
+
+int maxNumber = int.Parse(Console.ReadLine());
+int number = 1;
+
+while (number <= maxNumber)
+{
+    Console.WriteLine(number);
+    number = 2 * number + 1;
+}
+
