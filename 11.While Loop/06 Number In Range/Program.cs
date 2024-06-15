570ecc92294feb40e33edaef58536ce6f1845254
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-16 00:33:08 +0300

    Add files via upload

---
+++ 11.While Loop/06 Number In Range/Program.cs
@@ -0,0 +1,18 @@
+﻿//Write a program to read a number in the range [1 … 100]:
+
+//· Read an integer number from the console
+
+//· Check if the number is in the range [1 … 100]
+
+//o No(number is NOT in the range) à read a new number
+
+//o Yes(number is IN the range) à print the number and the program stops
+
+int num = int.Parse(Console.ReadLine());
+
+while (num <= 1 || num >= 100)
+{
+    num = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine(num);
