250a2af4dbfdaff6fd083bc7fd52cf31ba5155c3
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-16 00:32:04 +0300

    Add files via upload

---
+++ 11.While Loop/05.Sum Digits/Program.cs
@@ -0,0 +1,18 @@
+﻿//Write a program to sum the digits of given number:
+
+//· Read an integer positive number from the console
+
+//· Sum its digits and print the sum
+
+//Example: The number is 3451. Digits sum is 3 + 4 + 5 + 1 = 13
+
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+while (num != 0)
+{
+    int lastDigit = num % 10;
+    sum += lastDigit;
+    num /= 10;
+}
+
+Console.WriteLine(sum);
