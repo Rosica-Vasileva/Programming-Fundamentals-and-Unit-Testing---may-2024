39982e693a0600ba47786453819bf8cb9559a47d
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-19 01:37:16 +0300

    Add files via upload

---
+++ 12.Exercise:Loop/03.Biggest Number/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a program to find the biggest among given n numbers:
+
+//· Read an integer number n (the amount of input numbers) and n integer numbers from the console
+
+//· Find and print the biggest number
+
+int n = int.Parse(Console.ReadLine());
+int biggestNumber = int.MinValue;
+
+for (int i = 0; i < n; i++)
+{
+    int currentNum = int.Parse(Console.ReadLine());
+    if (currentNum > biggestNumber)
+    {
+        biggestNumber = currentNum;
+
+    }
+}
+
+Console.WriteLine(biggestNumber);
