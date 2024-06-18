c5b209197b2ea7605b808f01d91e99ad45b243df
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-18 23:50:45 +0300

    Add files via upload

---
+++ 12.Exercise:Loop/01.Power Of Number/Program.cs
@@ -0,0 +1,19 @@
+﻿//Write a program to calculate number raising to a certain power:
+
+//· Reads an integer number n from the console
+
+//· Reads an integer number p which represents power from the console
+
+//· Print the result of n to the power of p
+
+
+int n = int.Parse(Console.ReadLine());
+int p = int.Parse(Console.ReadLine());
+int sum = 1;
+
+for(int i = 0; i < p; i++)
+{
+    sum = sum * n;
+}
+
+Console.WriteLine(sum);
