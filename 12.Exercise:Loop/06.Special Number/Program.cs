ad445405d11017cb98901983a371b0d4174b828b
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-20 00:45:55 +0300

    Add files via upload

---
+++ 12.Exercise:Loop/06.Special Number/Program.cs
@@ -0,0 +1,36 @@
+﻿//Write a program to check if given number is special:
+
+//· Special numbers are divisible by all of their digits without remainder
+
+//· Reads an integer number and check whether it is a special number
+
+//· If the number IS special print: "{num} is special"
+
+//· If the number is NOT special print: "{num} is not special"
+
+//Note: There will not be numbers with digit 0 in them.
+
+int n = int.Parse(Console.ReadLine());
+int tempNum = n;
+bool isSpecial = true;
+
+while (tempNum > 0)
+{
+    int divisor = tempNum % 10;
+    if (n % divisor != 0)
+    {
+        isSpecial = false;
+        break;
+
+    }
+    tempNum /= 10;
+}
+if(isSpecial)
+{
+    Console.WriteLine($" {n} is special.");
+}
+
+else
+{
+    Console.WriteLine($" {n} is not special.");
+}
