4eb78536e9ae5decf11d30dfb3948d5804040eeb
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-19 23:46:28 +0300

    Add files via upload

---
+++ 12.Exercise:Loop/04.Vowel Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//Write a program to sum N vowels, according to the table below:
+
+//· Read an integer number N: the count of characters
+
+//· Read N characters and for each vowel character adds its value from the table to the result
+
+int n= int.Parse(Console.ReadLine());
+int sum = 0;
+
+for(int i = 0; i < n; i++)
+{
+    char letter = char.Parse(Console.ReadLine());
+    switch (letter)
+    {
+        case 'a':
+            sum += 1;
+            break;
+            case 'e':
+            sum += 2;
+            break;
+            case 'i':
+            sum += 3;
+            break;
+            case 'o':
+            sum += 4;
+            break;
+            case 'u':
+            sum += 5;
+            break;
+
+    }
+}
+Console.WriteLine(sum);
