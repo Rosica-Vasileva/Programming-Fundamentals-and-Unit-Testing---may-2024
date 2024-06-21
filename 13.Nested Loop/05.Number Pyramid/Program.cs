b20b4ff1fe280c189ae1b1b0e66b1353625e4677
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-21 23:45:36 +0300

    Add files via upload

---
+++ 13.Nested Loop/05.Number Pyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿//Write a program that:
+
+//· Reads an integer number n from the console
+
+//· Prints a pyramid of numbers as shown in the examples
+
+int n = int.Parse(Console.ReadLine());
+int currentNumber = 1;
+
+for(int row = 1; currentNumber <= n; row++)
+{
+    for(int col=1;col<=row && currentNumber <= n; col++)
+    {
+        Console.Write($"{currentNumber} ");
+        currentNumber++;
+    }
+    Console.WriteLine();
+}
