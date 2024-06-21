90db0d7d13e38591318b23144fc3e885d970fb7f
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-21 14:50:30 +0300

    Add files via upload

---
+++ 13.Nested Loop/01.Number from N to 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write a program that:
+
+//· Reads an integer number N from the console
+
+//· Prints the numbers from N to 1, each on separate line
+
+int n = int.Parse(Console.ReadLine());
+
+
+while (n >= 1)
+{
+    Console.WriteLine(n);
+    --n;
+}
