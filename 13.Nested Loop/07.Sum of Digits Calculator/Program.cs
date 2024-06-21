6560356cc0c01a4d6b3f7f3fc889773baa39caec
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-22 00:41:11 +0300

    Add files via upload

---
+++ 13.Nested Loop/07.Sum of Digits Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a program that:
+
+//· Continuously read integers until "End" is entered from the console
+
+//o Print the sum of digits for each integer, use the following format:
+
+//"Sum of digits = {sum}"
+
+//· Finally, print "Goodbye
+
+string input = "";
+while ((input = Console.ReadLine()) != "End")
+{
+    int n = int.Parse(input);
+    int sum = 0;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        sum += lastDigit;
+        n /= 10;
+    }
+    Console.WriteLine($"Sum of digits={sum}");
+}
+Console.WriteLine("Goodbay");
