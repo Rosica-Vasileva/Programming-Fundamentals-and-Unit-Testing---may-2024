60c7d6349dd8395369cf32619d2374c81118a54a
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-21 15:02:30 +0300

    Add files via upload

---
+++ 13.Nested Loop/02.Even Powers of 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Write a program that:
+
+//· Reads an integer number n from the console
+
+//· Prints on the console the number two on even powers in the range [0; n]
+
+//2 ≤ 2n: 20, 22, 24, 26, …, 2n.
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i =0;i<n;i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(Math.Pow(2, i));
+
+    }
+}
