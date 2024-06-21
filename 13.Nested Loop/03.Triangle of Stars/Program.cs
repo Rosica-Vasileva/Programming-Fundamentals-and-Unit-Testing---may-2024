9d6ebe2aaa3516247ee4e84cdd171265bf99e57d
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-21 15:33:16 +0300

    Add files via upload

---
+++ 13.Nested Loop/03.Triangle of Stars/Program.cs
@@ -0,0 +1,18 @@
+﻿//Write a program to print a triangle of stars like shown in the examples:
+
+//· Read the size (integer number) of a triangle from the console
+
+//· Print a triangle of stars
+
+
+int size = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= size; row++)
+{
+    for (int col = 1; col <= row; col++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
