a1fbef348e7bae01170f6d05c1a10a17ba3664e0
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-23 13:15:04 +0300

    Add files via upload

---
+++ 16.Methods/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿//Write a program that:
+
+//· Reads an integer number N from the console
+
+//· Create a method that prints the sign of an entered number N:
+
+//· Print:
+
+//o If the number is positive (N > 0): "The number {number} is positive."
+
+//o If the number is negative (N < 0): "The number {number} is negative."
+
+//o If the number is zero (N = 0): "The number {number} is zero."
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintSign(n);
+    }
+
+    static void PrintSign(int number)
+    {
+        if (number > 0)
+        {
+            Console.WriteLine($"The number {number} is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"The number {number} is negative.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {number} is zero.");
+        }
+    }
+}
