7b23f65114a9d4a1beb960d7487f23829ac212a8
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-24 00:29:21 +0300

    Add files via upload

---
+++ 16.Methods/04. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Write a program that:
+
+//· Reads two integer numbers from the console: width and length
+
+//· Create a method which returns rectangle area with given width and length
+
+//Hint: Rectangle area can be calculated when you multiply width and length of the rectangle.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Прочитане на ширината от конзолата
+        int width = int.Parse(Console.ReadLine());
+
+        // Прочитане на дължината от конзолата
+        int length = int.Parse(Console.ReadLine());
+
+        // Изчисляване на площта на правоъгълника
+        int area = CalculateRectangleArea(width, length);
+
+        // Печатане само на резултата
+        Console.WriteLine(area);
+    }
+
+    // Метод за изчисляване на площта на правоъгълника
+    static int CalculateRectangleArea(int width, int length)
+    {
+        return width * length;
+    }
+}
+
