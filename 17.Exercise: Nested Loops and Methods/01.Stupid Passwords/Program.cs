57ed78cd787be687d2a86de94b04a01b4931f49c
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-25 13:54:25 +0300

    Add files via upload

---
+++ 17.Exercise: Nested Loops and Methods/01.Stupid Passwords/Program.cs
@@ -0,0 +1,25 @@
+﻿//Write a program that:
+
+//· Reads an integer number N from the console
+
+//· Generates all possible passwords consisting of the following 3 parts:
+
+//o The first part is an even number in the range [2…N]
+
+//o The second digit is an odd number in the range [1…N]
+
+//o The third is the product of the first two
+
+//· The output holds all possible passwords
+
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= n; i += 2)
+{
+     for(int j = 1; j <= n; j += 2)
+    {
+        int t = i * j;
+        Console.Write($"{i}{j}{t} ");
+    }
+}
