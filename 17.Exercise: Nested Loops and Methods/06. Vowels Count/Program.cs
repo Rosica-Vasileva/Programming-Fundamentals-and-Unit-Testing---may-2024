e27e9e6d3b50638f2619a024a1149aa77532a8b1
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-26 23:34:35 +0300

    Add files via upload

---
+++ 17.Exercise: Nested Loops and Methods/06. Vowels Count/Program.cs
@@ -0,0 +1,54 @@
+﻿//Write a program that:
+
+//· Read a text (string) from the console
+
+//· Create a method that receives a text
+
+//· Find the count of the vowels contained in the text
+
+//· Print the count of the vowels in the text
+
+
+    static void Main(string[] args)
+    {
+        // Чете текст от конзолата
+        string input = Console.ReadLine();
+
+        // Извиква метода за броене на гласни
+        PrintVowelsCountInText(input);
+    }
+
+    static void PrintVowelsCountInText(string text)
+    {
+        int counter = 0;
+
+        // Цикъл за обхождане на текста
+        for (int i = 0; i < text.Length; i++)
+        {
+            char currentLetter = text[i];
+
+            // Проверка за гласна буква
+            switch (currentLetter)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                case 'Y':
+                    counter++;
+                    break;
+            }
+        }
+
+        // Извежда броя на гласните
+        Console.WriteLine(counter);
+    }
+
+
