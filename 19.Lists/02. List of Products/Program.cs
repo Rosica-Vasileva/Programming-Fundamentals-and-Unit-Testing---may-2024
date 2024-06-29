2e95db368b5f564f797f72377129a2edcec5fb28
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-06-29 15:45:58 +0300

    Add files via upload

---
+++ 19.Lists/02. List of Products/Program.cs
@@ -0,0 +1,22 @@
+﻿//Write a program that:
+
+//· Read an integer number n and n lines of products
+
+//· Print a numbered list of all the products ordered by name
+
+
+int n = int.Parse(Console.ReadLine());
+List<string> products = new List<string>();
+
+for(int i = 0; i < n; i++)
+{
+    string product = Console.ReadLine();
+    products.Add(product);
+}
+
+products.Sort();
+for(int i=0; i < products.Count; i++)
+{
+    Console.WriteLine($"{i+1}.{products[i]}");
+
+}
