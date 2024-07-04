acbf445cb66e07746a4fa3759122969121b1d079
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-07-05 02:07:22 +0300

    Add files via upload

---
+++ 21.Exam Preparation I/02.Middle Elements/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int[] numbers = Console.ReadLine()
+    .Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int middleIndex = numbers.Length / 2;
+int firstNumber = numbers[middleIndex];
+int secondNumber = numbers[middleIndex - 1];
+
+int sum = firstNumber + secondNumber;
+double result = sum / 2.0;
+Console.WriteLine($"{result:F2}");
